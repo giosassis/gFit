ae0926fb7625582e709ec015f6faa0698336148a
giovanasantannadeassis <giovana.sant@hotmail.com>
2023-08-20 18:07:42 -0300

    bugfix aws ses

--- Services/Implementation/EmailService.cs
+++ Services/Implementation/EmailService.cs
@@ -1,30 +1,28 @@
 ﻿using Amazon;
 using Amazon.SimpleEmail;
 using Amazon.SimpleEmail.Model;
-namespace gFit.Services.Implementation
+
+public class EmailService : IEmailService
 {
-    public class EmailService : IEmailService
+    private readonly string _awsAccessKeyId = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID");
+    private readonly string _awsSecretKey = Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY");
+    private readonly string authorizedEmail = Environment.GetEnvironmentVariable("AuthorizedEmail");
+    private readonly RegionEndpoint _region = RegionEndpoint.USEast1; // Ajuste para a região correta
+
+    public async Task SendEmailAsync(string toEmail, string subject, string message)
     {
-        private readonly string _awsAccessKeyId = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID");
-        private readonly string _awsSecretKey = Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY");
-        private readonly string authorizedEmail = Environment.GetEnvironmentVariable("AuthorizedEmail");
-        private readonly RegionEndpoint _region = RegionEndpoint.USEast1;
-
-        public async Task SendEmailAsync(string toEmail, string subject, string message)
+        var sendRequest = new SendEmailRequest
         {
-            var sendRequest = new SendEmailRequest
+            Source = authorizedEmail,
+            Destination = new Destination { ToAddresses = new List<string> { toEmail } },
+            Message = new Message
             {
-                Source = authorizedEmail,
-                Destination = new Destination { ToAddresses = new List<string> { toEmail } },
-                Message = new Message
-                {
-                    Subject = new Content(subject),
-                    Body = new Body { Text = new Content(message) }
-                }
-            };
+                Subject = new Content(subject),
+                Body = new Body { Text = new Content(message) }
+            }
+        };
 
-            using var client = new AmazonSimpleEmailServiceClient(_awsAccessKeyId, _awsSecretKey, _region);
-            await client.SendEmailAsync(sendRequest);
-        }
+        using var client = new AmazonSimpleEmailServiceClient(_awsAccessKeyId, _awsSecretKey, _region);
+        await client.SendEmailAsync(sendRequest);
     }
 }
